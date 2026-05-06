7d961c757e75f963d33ec557e32dcaa0d9c1a767
ThreeeJ <2171266@hansung.ac.kr>
2026-05-06 09:43:02 +0900

    fix: CandidateRes DTO 수정 (위/경도 추가)

--- src/main/java/org/hansung/zigma/domain/promise/web/dto/CandidateRes.java
+++ src/main/java/org/hansung/zigma/domain/promise/web/dto/CandidateRes.java
@@ -6,6 +6,8 @@ public record CandidateRes(
         Long id,
         String name,
         String category,
+        Double latitude,    // 위도
+        Double longitude,   // 경도
         String address,
         Double distance,
         Boolean isConfirmed
@@ -21,6 +23,8 @@ public record CandidateRes(
                 candidate.getId(),
                 candidate.getName(),
                 candidate.getCategory(),
+                candidate.getLatitude(),
+                candidate.getLongitude(),
                 candidate.getAddress(),
                 120.0,
                 candidate.getIsConfirmed()
